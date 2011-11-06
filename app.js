b0cf84824e503ecfa65e66b2641fb5023bea5323
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-11-06 19:35:26 +0900

    Improve: Inserts tweets into MongoDB

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express')
   , twitter = require('ntwitter')
   , config = require('confu')(__dirname, 'config.json')
   , twit = new twitter(config.twitter)
+  , mongodb = require('mongodb')
   , EntitiesParser = require('./lib/entities_parser')
   , entitiesParser = new EntitiesParser
   , Tweet = require('./lib/tweet');
@@ -37,34 +38,55 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(3000, function () {
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
-twit.stream('user', function(stream) {
-  stream.on('data', function (data) {
-    var tweet, entities;
-    if ('text' in data && 'entities' in data) {
-      entities = entitiesParser.parse(data.entities);
-      tweet = new Tweet({
-        "status_id": data.id
-      , "text": data.text
-      , "urls": entities.urls
-      , "hashtags": entities.hashtags
-      , "user_id": data.user.id
-      , "screen_name": data.user.screen_name
-      , "profile_image_url": data.user.profile_image_url
-      , "created_at": new Date(Date.parse(data.created_at))
-      });
-      if (tweet.hasUrl()) {
-        io.sockets.emit('tweet', tweet.data);
+  new mongodb.Db(
+    config.mongodb.database,
+    new mongodb.Server(config.mongodb.host, config.mongodb.port, {})
+  ).open(function (err, db) {
+    if (err) {
+      console.error('Failed to open database!');
+      console.error(err);
+      throw err;
+    };
+
+    db.collection('tweets', function (err, collection) {
+      if (err) {
+        console.error('Failed to open collection!');
+        console.error(err);
+        throw err;
       }
-    }
-  });
-});
 
-io.sockets.on('connection', function (socket) {
-  console.log('Connected');
-  socket.on('disconnect', function () {
-    console.log('Disconnected.');
+      twit.stream('user', function(stream) {
+        stream.on('data', function (data) {
+          var tweet, entities;
+          if ('text' in data && 'entities' in data) {
+            entities = entitiesParser.parse(data.entities);
+            tweet = new Tweet({
+              "status_id": data.id
+            , "text": data.text
+            , "urls": entities.urls
+            , "hashtags": entities.hashtags
+            , "user_id": data.user.id
+            , "screen_name": data.user.screen_name
+            , "profile_image_url": data.user.profile_image_url
+            , "created_at": new Date(Date.parse(data.created_at))
+            });
+            if (tweet.hasUrl()) {
+              io.sockets.emit('tweet', tweet.data);
+              collection.insert(tweet.data);
+            }
+          }
+        });
+      });
+
+      io.sockets.on('connection', function (socket) {
+        console.log('Connected');
+        socket.on('disconnect', function () {
+          console.log('Disconnected.');
+        });
+      });
+    });
   });
 });
