df36eac59d65bbc864d569b4d13c8c828730085b
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-11-06 15:33:59 +0900

    1st commit.

---
+++ app.js
@@ -0,0 +1,54 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , twitter = require('ntwitter')
+  , config = require('confu')(__dirname, 'config.json')
+  , twit = new twitter(config.twitter);
+
+var app = module.exports = express.createServer()
+  , io = require('socket.io').listen(app);
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+twit.stream('user', function(stream) {
+  stream.on('data', function (data) {
+    if ('text' in data && 'entities' in data) {
+      console.log(data.text);
+      console.dir(data.entities);
+    }
+  });
+});
+
+io.sockets.on('connection', function (socket) {
+  console.log('Connected');
+  socket.on('disconnect', function () {
+    console.log('Disconnected.');
+  });
+});
