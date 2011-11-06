4616032fe24e77dc9a6ff20e820bb567b0a40410
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-11-06 21:59:27 +0900

    Change: Port number is configurable.

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000, function () {
+app.listen(config.app.port, function () {
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
   new mongodb.Db(
