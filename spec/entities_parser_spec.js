e5a724e905403953a2d058c2296b88e7731743a1
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-11-06 16:34:20 +0900

    Change: Re-structured spec.

--- spec/entities_parser_spec.js
+++ spec/entities_parser_spec.js
@@ -7,31 +7,33 @@ describe('EntitiesParser', function () {
     parser = new EntitiesParser;
   });
 
-  it('should parse expanded_url as array', function () {
-    var result = parser.parse({
-      'urls':[
-        {"expanded_url":'http://example.com/'}
-      ]
+  describe('.parse()', function () {
+    it('should parse expanded_url as array', function () {
+      var result = parser.parse({
+        'urls':[
+          {"expanded_url":'http://example.com/'}
+        ]
+      });
+      expect(result.urls).toEqual(['http://example.com/']);
     });
-    expect(result.urls).toEqual(['http://example.com/']);
-  });
 
-  it('should parse expanded_url uniquely', function () {
-    var result = parser.parse({
-      'urls':[
-        {"expanded_url":'http://example.com/'},
-        {"expanded_url":'http://example.com/'},
-        {"expanded_url":'http://example.net/'}
-      ]
+    it('should parse expanded_url uniquely', function () {
+      var result = parser.parse({
+        'urls':[
+          {"expanded_url":'http://example.com/'},
+          {"expanded_url":'http://example.com/'},
+          {"expanded_url":'http://example.net/'}
+        ]
+      });
+      expect(result.urls).toEqual([
+        'http://example.com/',
+        'http://example.net/',
+      ]);
     });
-    expect(result.urls).toEqual([
-      'http://example.com/',
-      'http://example.net/',
-    ]);
-  });
 
-  it('should return empty array if it has no URLs', function () {
-    var result = parser.parse({'urls':[]});
-    expect(result.urls).toEqual([]);
+    it('should return empty array if it has no URLs', function () {
+      var result = parser.parse({'urls':[]});
+      expect(result.urls).toEqual([]);
+    });
   });
 });
